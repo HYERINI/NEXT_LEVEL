9a2f11c4dbabbed1b8dc5b1e2e12d21f5e2301a4
HYERINI <rinlee2001@naver.com>
2021-07-11 16:28:24 +0900

    포기

--- crawling.py
+++ crawling.py
@@ -1,24 +1,36 @@
-from typing import no_type_check
+# from typing import no_type_check
+# import requests
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+
+# #세션 만들기
+# session=requests.session()
+
+# #로그인 하는 페이지의 general-requestURL에서 url 가져옴
+# url="https://ecampus.smu.ac.kr/login/index.php"
+
+# #가져오고 싶은 데이터 (Form data)
+# data = {
+#     "username" : "아이디",
+#     "password" : "비밀번호"
+# }
+# response=session.post(url, data=data)
+# response.raise_for_status()
+
+# #나의강좌 접근
+# url="https://ecampus.smu.ac.kr/local/ubion/user/"
+# response=session.get(url)
+# response.raise_for_status()
+
 import requests
-from selenium import webdriver
 from bs4 import BeautifulSoup
 
-#세션 만들기
-session=requests.session()
+url = 'https://ecampus.smu.ac.kr/mod/assign/index.php?id=60021'
 
-#로그인 하는 페이지의 general-requestURL에서 url 가져옴
-url="https://ecampus.smu.ac.kr/login/index.php"
+response = requests.get(url)
 
-#가져오고 싶은 데이터 (Form data)
-data = {
-    "username" : "아이디",
-    "password" : "비밀번호"
-}
-response=session.post(url, data=data)
-response.raise_for_status()
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
 
-#나의강좌 접근
-url="https://ecampus.smu.ac.kr/local/ubion/user/"
-response=session.get(url)
-response.raise_for_status()
-#쉬바 모르겠어
+target = soup.find('table', {'class' : 'generaltable table table-bordered'})
+print(target)
