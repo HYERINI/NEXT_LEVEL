56b32f9643486fdefcddc87d3907012104fb8023
HYERINI <rinlee2001@naver.com>
2021-08-03 18:08:22 +0900

    discord bot

--- 21.디코봇.py
+++ 21.디코봇.py
@@ -1,28 +1,143 @@
-import asyncio
-import discord
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as 
+이캠대신
+872029574748319754
+https://lostark.game.onstove.com/Profile/Character/11
+Lv.50
+https://lostark.game.onstove.com/Profile/Character/%40749631199517081751%3E%20%3A%20%EB%A0%88%EB%B2%A8%2211%22Lv.50
+Ignoring exception in on_message
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 24, in on_message
+    html = urlopen(url)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 222, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 531, in open
+    response = meth(req, response)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 640, in http_response
+    response = self.parent.error(
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 569, in error
+    return self._call_chain(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 502, in _call_chain
+    result = func(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 649, in http_error_default
+    raise HTTPError(req.full_url, code, msg, hdrs, fp)
+urllib.error.HTTPError: HTTP Error 404: Not Found
+https://lostark.game.onstove.com/Profile/Character/1
+Ignoring exception in on_message
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 27, in on_message
+    tmpContent = bsObject.find_all("div", {"class":"level-info__item"})[0].find_all("span")[1].text
+IndexError: list index out of range
+https://lostark.game.onstove.com/Profile/Character/11
+Lv.50
+https://lostark.game.onstove.com/Profile/Character/%40749631199517081751%3E%20%3A%20%EB%A0%88%EB%B2%A8%2211%22Lv.50
+Ignoring exception in on_message
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 24, in on_message
+    html = urlopen(url)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 222, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 531, in open
+    response = meth(req, response)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 640, in http_response
+    response = self.parent.error(
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 569, in error
+    return self._call_chain(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 502, in _call_chain
+    result = func(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 649, in http_error_default
+    raise HTTPError(req.full_url, code, msg, hdrs, fp)
+urllib.error.HTTPError: HTTP Error 404: Not Found
+https://lostark.game.onstove.com/Profile/Character/11
+Lv.50
+https://lostark.game.onstove.com/Profile/Character/%40749631199517081751%3E%20%3A%20%EB%A0%88%EB%B2%A8%2211%22Lv.50
+Ignoring exception in on_message
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 24, in on_message
+    html = urlopen(url)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 222, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 531, in open
+    response = meth(req, response)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 640, in http_response
+    response = self.parent.error(
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 569, in error
+    return self._call_chain(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 502, in _call_chain
+    result = func(*args)
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\urllib\request.py", line 649, in http_error_default
+    raise HTTPError(req.full_url, code, msg, hdrs, fp)
+urllib.error.HTTPError: HTTP Error 404: Not Found
 
-client = discord.Client()
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
 
-# 생성된 토큰을 입력해준다.
-token = "토큰"
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
 
-# 봇이 구동되었을 때 보여지는 코드
-@client.event
-async def on_ready():
-    print("다음으로 로그인합니다")
-    print(client.user.name)
-    print(client.user.id)
-    print("================")
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as 
+이캠대신
+872029574748319754
+===========
+Ignoring exception in on_ready
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 18, in on_ready
+    await client.change_presence(game=discord.Game(name="반갑습니다 :D", type=1))
+TypeError: change_presence() got an unexpected keyword argument 'game'
+Ignoring exception in on_message
+Traceback (most recent call last):
+  File "C:\Users\이혜린\AppData\Local\Programs\Python\Python38-32\lib\site-packages\discord\client.py", line 343, in _run_event
+    await coro(*args, **kwargs)
+  File "C:/Users/이혜린/Desktop/discordbot.py", line 30, in on_message
+    await client.send_message(channel, '커맨드') #봇은 해당 채널에 '커맨드' 라고 말합니다.
+AttributeError: 'Client' object has no attribute 'send_message'
 
-# 봇이 특정 메세지를 받고 인식하는 코드
-@client.event
-async def on_message(message):
-    # 메세지를 보낸 사람이 봇일 경우 무시한다
-    if message.author.bot:
-        return None
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
 
-    if message.content.startswith('!안녕'):
-        channel = message.channel
-        await channel.send('반가워!')
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
 
-client.run('토큰입력')
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
+
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
+
+================== RESTART: C:/Users/이혜린/Desktop/discordbot.py =================
+Logged in as
+이캠대신
+872029574748319754
+------
